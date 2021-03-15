ebc6f18dc1d6228b48faa3eb6bd9556d15947189
Jakub Bielecki <47531708+jabielecki@users.noreply.github.com>
2021-03-15 11:29:25 +0100

    feat(vmseries): conditional boot diagnostics

--- modules/vmseries/main.tf
+++ modules/vmseries/main.tf
@@ -86,7 +86,7 @@ resource "azurerm_virtual_machine" "this" {
   # After converting to azurerm_linux_virtual_machine, an empty block boot_diagnostics {} will use managed storage. Want.
   # 2.36 in required_providers per https://github.com/terraform-providers/terraform-provider-azurerm/pull/8917
   boot_diagnostics {
-    enabled     = true
+    enabled     = (var.bootstrap_storage_account != null)
     storage_uri = var.bootstrap_storage_account.primary_blob_endpoint
   }
 
