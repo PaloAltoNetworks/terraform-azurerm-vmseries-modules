417623058a61094eac5048c498e50e5d6d6c4061
Jakub Bielecki <47531708+jabielecki@users.noreply.github.com>
2021-03-18 13:09:09 +0100

    fix(examples/vmseries): random with minima
    Without min_upper, etc, sometimes the generated password was not
    accepted by Azure. Randomly, sometimes password had no upper chars etc.

--- examples/transit_vnet_common/main.tf
+++ examples/transit_vnet_common/main.tf
@@ -11,7 +11,10 @@ locals {
 
 resource "random_password" "password" {
   length           = 16
-  special          = true
+  min_lower        = 16 - 4
+  min_numeric      = 1
+  min_special      = 1
+  min_upper        = 1
   override_special = "_%@"
 }
 
