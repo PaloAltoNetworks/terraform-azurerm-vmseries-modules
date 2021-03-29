9226229c9dde9123201c6535aa693ae0913ddd03
Jakub Bielecki <47531708+jabielecki@users.noreply.github.com>
2021-03-29 14:14:36 +0200

    fix(examples/tvc): required_version 0.13
    Set required_version 0.13 because the example uses module depends_on
    syntax forbidden on 0.12.
    
    Retain azurerm pinned exactly to 2.42 for this example to run cleanly.
    This avoids unpredictable warnings when running on the newer 2.xx versions.

--- examples/transit_vnet_common/versions.tf
+++ examples/transit_vnet_common/versions.tf
@@ -1,13 +1,18 @@
 terraform {
-  required_version = ">=0.12.29, <0.14"
+  required_version = ">=0.13, <0.14"
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~>2.42"
+      version = "=2.42"
     }
     random = {
       source  = "hashicorp/random"
       version = "~>3.0"
     }
   }
+}
+
+provider "azurerm" {
+  features {}
 }
