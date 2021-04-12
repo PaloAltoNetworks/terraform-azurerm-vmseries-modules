e9bb9b81040b806d5607fb8dbef5745f1fa0751b
Konrad Bechler <kbechler@paloaltonetworks.com>
2021-04-12 14:02:40 +0200

    small change to be consistent with TERRAM-50

--- examples/vmseries_scaleset/main.tf
+++ examples/vmseries_scaleset/main.tf
@@ -25,12 +25,11 @@ module "vnet" {
 
   virtual_network_name    = var.virtual_network_name
   resource_group_name     = azurerm_resource_group.this.name
+  location                = var.location
   address_space           = var.address_space
   network_security_groups = var.network_security_groups
   route_tables            = var.route_tables
   subnets                 = var.subnets
-
-  depends_on = [azurerm_resource_group.this]
 }
 
 
