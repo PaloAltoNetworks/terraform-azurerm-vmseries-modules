189498342567049df2ee93b8d8c5687e6b12aeca
damianfedeczko <dfedeczko@paloaltonetworks.com>
2021-04-16 13:55:06 +0200

    Fix issue #81 with routes (#83)

--- modules/vnet/main.tf
+++ modules/vnet/main.tf
@@ -80,11 +80,12 @@ resource "azurerm_route" "this" {
     for route in local.route : "${route.route_table_name}-${route.name}" => route
   }
 
-  name                = each.value.name
-  resource_group_name = var.resource_group_name
-  route_table_name    = azurerm_route_table.this[each.value.route_table_name].name
-  address_prefix      = each.value.route.address_prefix
-  next_hop_type       = each.value.route.next_hop_type
+  name                   = each.value.name
+  resource_group_name    = var.resource_group_name
+  route_table_name       = azurerm_route_table.this[each.value.route_table_name].name
+  address_prefix         = each.value.route.address_prefix
+  next_hop_type          = each.value.route.next_hop_type
+  next_hop_in_ip_address = try(each.value.route.next_hop_in_ip_address, null)
 }
 
 resource "azurerm_subnet_network_security_group_association" "this" {
