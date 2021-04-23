8e1a2a877468b98837e3e65e3ffe7b3ff9c143c3
Jakub Bielecki <47531708+jabielecki@users.noreply.github.com>
2021-04-23 13:38:55 +0200

    feat(loadbalancer): Add output `frontend_combined_rules`
    Provide full output that could be easily converted into a sequence of
    NSG rules. This way users can have their own logic to create NSGs.
    The NSG rules auto-generated by the module are not suitable for
    everyone, hence users have this escape hatch.

--- modules/loadbalancer/outputs.tf
+++ modules/loadbalancer/outputs.tf
@@ -4,6 +4,31 @@ output "backend_pool_id" {
 }
 
 output "frontend_ip_configs" {
-  value       = local.frontend_ip_configs
-  description = "The Frontend configs of the loadbalancer."
+  value       = local.output_ips
+  description = "Map of IP addresses, one per each entry of `frontend_ips` input. Contains public IP address for the frontends that have it, private IP address otherwise."
+}
+
+output "frontend_combined_rules" {
+  value       = local.output_rules
+  description = <<-EOF
+  Map of all rules of all load balancer's frontends combined.
+  The map entries are intended to be easily convertable into an NSG rule, hence each entry
+  contains `port`, `protocol`,  `frontend_ip`, and numerical sequential `index`.
+  The `frontend_ip` is the same as as returned by output `frontend_ip_configs`.
+
+  Full example:
+
+  ```hcl
+  {
+    "frontend01-balancessh" = {
+      "fipkey" = "frontend01"
+      "frontend_ip" = "34.34.34.34"
+      "index" = 0
+      "port" = 22
+      "protocol" = "tcp"
+      "rulekey" = "balancessh"
+    }
+  }
+  ```
+  EOF
 }
