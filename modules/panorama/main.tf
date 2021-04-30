742e83b71184c5b85e426acafa5d0db792e69d87
Steve Alsop <me@stevenalsop.com>
2021-04-30 10:21:39 +0100

    fix(examples/panorama): change Public IP to SKU "Standard" (#96)
    Fix error when deploying to zone as defaulting to basic public ip.
    Follow what Azure says: "Use Standard SKU public IP for Availability Zone scenarios".
    
    Co-authored-by: Jakub Bielecki <47531708+jabielecki@users.noreply.github.com>

--- modules/panorama/main.tf
+++ modules/panorama/main.tf
@@ -6,6 +6,7 @@ resource "azurerm_public_ip" "this" {
   location            = var.location
   resource_group_name = var.resource_group_name
   allocation_method   = "Static"
+  sku                 = "Standard"
 
   tags = var.tags
 }
