1baecff4cf13375a256b51c672494438a617b727
Jakub Bielecki <47531708+jabielecki@users.noreply.github.com>
2021-09-21 11:50:28 +0200

    feat(bootstrap): add the new input `files_md5` to facilitate dynamic files
    Add the new input `files_md5`. It is optional, to retain the backward
    compatibility.
    
    The old problem was that the `files` could not be auto-generated on the
    same Apply as this module. The builtin `md5(file(x))`
    fails hard if the file does not exist during Validate phase, which is
    earlier than Plan or Apply.
    
    For users it is a hurdle on the way to two features:
    
    - to read the secrets out of some vault within Terraform, which would be
    very useful for authcodes.
    - to read the vm-auth-key from a Panorama.
    
    Now, users are able to generate the `files` on the spot, provided that
    they supply the `files_md5` for them. If the `files` entries correspond
    to the files that actually exist before the Terraform run, nothing
    changes for these (that's the old way used everywhere in the examples).

--- modules/bootstrap/main.tf
+++ modules/bootstrap/main.tf
@@ -54,8 +54,8 @@ resource "azurerm_storage_share_file" "this" {
   source           = replace(each.key, "/CalculateMe[X]${random_id.this[each.key].id}/", "CalculateMeX${random_id.this[each.key].id}")
   # Live above is equivalent to:   `source = each.key`  but it re-creates the file every time the content changes.
   # The replace() is not actually doing anything, except tricking Terraform to destroy a resource.
-  # There is a field content_md5 designed specifically for that. But I see a bug in the provider: 
-  # When content_md5 is changed, the re-upload seemingly succeeds, result being however a totally empty file (size zero).
+  # There is a field content_md5 designed specifically for that. But I see a bug in the provider (last seen in 2.76):
+  # When content_md5 changes the re-uploading seemingly succeeds, result being however a totally empty file (size zero).
   # Workaround: use random_id above to cause the full destroy/create of a file.
   depends_on = [azurerm_storage_share_directory.config, azurerm_storage_share_directory.nonconfig]
 }
@@ -65,7 +65,7 @@ resource "random_id" "this" {
 
   keepers = {
     # Re-randomize on every content/md5 change. It forcibly recreates all users of this random_id.
-    md5 = md5(file(each.key))
+    md5 = try(var.files_md5[each.key], md5(file(each.key)))
   }
   byte_length = 8
 }
--- modules/bootstrap/variables.tf
+++ modules/bootstrap/variables.tf
@@ -42,6 +42,12 @@ variable "files" {
   type        = map(string)
 }
 
+variable "files_md5" {
+  description = "Optional map of MD5 hashes. Normally the map should be empty, it is intended to be used for the files created in the same Terraform run as this module, which would not be automatically hashed. The keys of the map should be same or all of the keys of `files` input, while the values should be MD5 hashes of file contents. For example `{\"dir/my.txt\" = \"6f7ce3191b50a58cc13e751a8f7ae3fd\"}`"
+  default     = {}
+  type        = map(string)
+}
+
 variable "storage_share_name" {
   description = "Name of storage share to be created that holds `files` for bootstrapping."
   default     = "bootstrapshare"
