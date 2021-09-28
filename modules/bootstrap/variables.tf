a1bdac037b23f301c4cbb94efeea1b2700616168
Jakub Bielecki <47531708+jabielecki@users.noreply.github.com>
2021-09-28 14:37:04 +0200

    bootstrap more documentation

--- modules/bootstrap/variables.tf
+++ modules/bootstrap/variables.tf
@@ -37,13 +37,22 @@ variable "existing_storage_account" {
 }
 
 variable "files" {
-  description = "Map of all files to copy to bucket. The keys are local paths, the values are remote paths. Always use slash `/` as directory separator (unix-like), not the backslash `\\`. For example `{\"dir/my.txt\" = \"config/init-cfg.txt\"}`"
+  description = <<-EOF
+  Map of all files to copy to bucket. The keys are local paths, the values are remote paths. Always use slash `/` as
+  directory separator (unix-like), not the backslash `\`. For example `{"dir/my.txt" = "config/init-cfg.txt"}`
+  EOF
   default     = {}
   type        = map(string)
 }
 
 variable "files_md5" {
-  description = "Optional map of MD5 hashes of file contents. Normally the map could be all empty, because all the files that exist before the `terraform apply` will have their hashes auto-calculated. This input is necessary only for the selected files which are created/modified within the same Terraform run as this module. The keys of the map should be identical with selected keys of the `files` input, while the values should be MD5 hashes of the contents of that file. For example `{\"dir/my.txt\" = \"6f7ce3191b50a58cc13e751a8f7ae3fd\"}`"
+  description = <<-EOF
+  Optional map of MD5 hashes of file contents. Normally the map could be all empty, because all the files that exist
+  before the `terraform apply` will have their hashes auto-calculated. This input is necessary only for the selected
+  files which are created/modified within the same Terraform run as this module. The keys of the map should be identical
+  with selected keys of the `files` input, while the values should be MD5 hashes of the contents of that file.
+  For example `{"dir/my.txt" = "6f7ce3191b50a58cc13e751a8f7ae3fd"}`
+  EOF
   default     = {}
   type        = map(string)
 }
