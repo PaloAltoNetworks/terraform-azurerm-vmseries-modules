a562bca2691ea1f3026f8ff99be40802ee31fca1
Łukasz Pawlęga <42772730+FoSix@users.noreply.github.com>
2023-02-22 12:17:30 +0100

    ci: release CI/CD (#210)

--- scripts/run.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-# run.sh - Run the usual pre-commit checks.
-
-set -euo pipefail
-
-pre-commit run --all-files terraform_fmt 
-pre-commit run --all-files terraform_docs 
-pre-commit run --all-files terraform_tflint
