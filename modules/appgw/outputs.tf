84d1b72f7d3e2b62a7ae29c35d84daec9d1978e6
Sebastian Czech <sebaczech@gmail.com>
2023-04-04 07:38:52 +0200

    feat(module/appgw): add identifier of the Application Gateway backend address pool (#250)

--- modules/appgw/outputs.tf
+++ modules/appgw/outputs.tf
@@ -7,3 +7,8 @@ output "public_domain_name" {
   description = "Public domain name assigned to the Application Gateway."
   value       = azurerm_public_ip.this.fqdn
 }
+
+output "application_gateway_backend_address_pool_id" {
+  description = "The identifier of the Application Gateway backend address pool."
+  value       = one(azurerm_application_gateway.this.backend_address_pool).id
+}
