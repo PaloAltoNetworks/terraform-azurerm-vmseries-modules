2655a13716bbdc720b8341b9a2dffb40e3bb889d
Horia Gunica <Horia.Gunica@gmail.com>
2023-07-18 14:51:59 +0300

    Modified variables description

--- modules/vnet_peering/variables.tf
+++ modules/vnet_peering/variables.tf
@@ -8,18 +8,13 @@ variable "local_peer_config" {
   description = <<-EOF
   A map that contains the local peer configuration.
   Mandatory Values: 
-  - `vnet_name` (`string`) : the local peer VNET name.
-  - `resource_group_name (`string`) : the resource group name of the local peer
-  - `allow_virtual_network_access (`bool`) : allows communication between the two peering VNETs
-  - `allow_forwarded_traffic` (`bool`) : allows traffic forwarded from the remote VNET but not originated from within it
-  - `allow_gateway_transit` (`bool`) : controls the learning of routes from local VNET (gateway or route server) into the remote VNET. Must be true if `use_remote_gateways` is `true` for remote peer
-  - `use_remote_gateways` (`bool`) : controls the learning of routes from the remote VNET (gateway or route server) into the local VNET
-
-  <details>
-  <summary>Optional</summary>
-  - `name` (string) : the name of the local VNET peering
-  </details>
-
+  - `vnet_name`                   - (`string`, required) : the local peer VNET name.
+  - `resource_group_name          - (`string`, required) : the resource group name of the local peer
+  - `allow_virtual_network_access - (`bool`, required) : allows communication between the two peering VNETs
+  - `allow_forwarded_traffic`     - (`bool`, required) : allows traffic forwarded from the remote VNET but not originated from within it
+  - `allow_gateway_transit`       - (`bool`, required) : controls the learning of routes from local VNET (gateway or route server) into the remote VNET. Must be true if `use_remote_gateways` is `true` for remote peer
+  - `use_remote_gateways`         - (`bool`, required) : controls the learning of routes from the remote VNET (gateway or route server) into the local VNET
+  - `name`                        - (`string`, optional, defaults to `<var.name_prefix><var.local_peer_config.vnet_name>-to-<var.remote_peer_config.vnet_name>`) : the name of the local VNET peering
   EOF
   type        = map(any)
 }
@@ -27,19 +22,14 @@ variable "local_peer_config" {
 variable "remote_peer_config" {
   description = <<-EOF
   A map that contains the remote peer configuration.
-  Mandatory Values : 
-  - `vnet_name` (`string`) : the remote peer VNET name.
-  - `resource_group_name (`string`) : the resource group name of the remote peer
-  - `allow_virtual_network_access (`bool`) : allows communication between the two peering VNETs
-  - `allow_forwarded_traffic` (`bool`) : allows traffic forwarded from the local VNET but not originated from within it
-  - `allow_gateway_transit` (`bool`) : controls the learning of routes from remote VNET (gateway or route server) into the local VNET. Must be true if `use_remote_gateways` is `true` for local peer
-  - `use_remote_gateways` (`bool`) : controls the learning of routes from the local VNET (gateway or route server) into the remote VNET
-
-  <details>
-  <summary>Optional</summary>
-  - `name` (string) : the name of the remote VNET peering
-  </details>
-
+  Mandatory Values :
+  - `vnet_name`                   - (`string`, required) : the remote peer VNET name.
+  - `resource_group_name          - (`string`, required) : the resource group name of the remote peer
+  - `allow_virtual_network_access - (`bool`, required) : allows communication between the two peering VNETs
+  - `allow_forwarded_traffic`     - (`bool`, required) : allows traffic forwarded from the local VNET but not originated from within it
+  - `allow_gateway_transit`       - (`bool`, required) : controls the learning of routes from remote VNET (gateway or route server) into the local VNET. Must be true if `use_remote_gateways` is `true` for local peer
+  - `use_remote_gateways`         - (`bool`, required) : controls the learning of routes from the local VNET (gateway or route server) into the remote VNET
+  - `name`                        - (`string`, optional, defaults to `<var.name_prefix><var.remote_peer_config.vnet_name>-to-<var.local_peer_config.vnet_name>`) : the name of the local VNET peering
   EOF
   type        = map(any)
 }
