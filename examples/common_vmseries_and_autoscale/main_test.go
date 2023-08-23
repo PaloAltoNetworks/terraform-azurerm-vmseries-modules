ff70402a30a69fe5ab2320a9d634e99fddd027a6
Łukasz Pawlęga <lpawlega@paloaltonetworks.com>
2023-08-23 13:26:05 +0200

    add terratest to common_vmseries_and_autoscale

---
+++ examples/common_vmseries_and_autoscale/main_test.go
@@ -0,0 +1,46 @@
+package common_vmseries_and_autoscale
+
+import (
+	"os"
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/logger"
+	"github.com/gruntwork-io/terratest/modules/terraform"
+
+	"github.com/PaloAltoNetworks/terraform-modules-vmseries-tests-skeleton/pkg/testskeleton"
+)
+
+func TestDeploy(t *testing.T) {
+	// prepare random prefix
+	randomNames := testskeleton.GenerateAzureRandomNames()
+
+	// define options for Terraform
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		TerraformDir: ".",
+		VarFiles:     []string{"example.tfvars"},
+		Vars: map[string]interface{}{
+			"name_prefix":         randomNames.NamePrefix,
+			"resource_group_name": randomNames.ResourceGroupName,
+		},
+		Logger:               logger.Default,
+		Lock:                 true,
+		Upgrade:              true,
+		SetVarsAfterVarFiles: true,
+	})
+
+	// prepare list of items to check
+	assertList := []testskeleton.AssertExpression{}
+
+	// if DO_APPLY is not empty and equal true, then Terraform apply is used, in other case only Terraform plan
+	if os.Getenv("DO_APPLY") == "true" {
+		// deploy test infrastructure and verify outputs and check if there are no planned changes after deployment
+		testskeleton.DeployInfraCheckOutputsVerifyChanges(t, terraformOptions, assertList)
+	} else {
+		// plan test infrastructure and verify outputs
+		testskeleton.PlanInfraCheckErrors(t, terraformOptions, assertList, "No errors are expected")
+	}
+}
+
+func TestValidate(t *testing.T) {
+	testskeleton.ValidateCode(t, nil)
+}
