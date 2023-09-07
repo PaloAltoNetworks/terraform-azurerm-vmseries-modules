20e456b65a11d6453bd16327de291cf3d1b1c543
Łukasz Pawlęga <42772730+FoSix@users.noreply.github.com>
2023-09-07 12:19:15 +0200

    feat!: introduce basic Terratest based tests, unified Makefile and ChatOPS like workflows (#304)

---
+++ modules/natgw/main_test.go
@@ -0,0 +1,11 @@
+package natgw
+
+import (
+	"testing"
+
+	"github.com/PaloAltoNetworks/terraform-modules-vmseries-tests-skeleton/pkg/testskeleton"
+)
+
+func TestValidate(t *testing.T) {
+	testskeleton.ValidateCode(t, nil)
+}
