1ba930ec3424df7cf8bf26232ad578d6ec358cd1
Łukasz Pawlęga <lpawlega@paloaltonetworks.com>
2023-09-29 09:35:45 +0200

    1st module tests

--- tests/modules/vnet/main.tf
+++ tests/modules/vnet/main.tf
@@ -1,30 +1,20 @@
 # Create or source the Resource Group.
 resource "azurerm_resource_group" "this" {
-  count    = var.create_resource_group ? 1 : 0
   name     = "${var.name_prefix}${var.resource_group_name}"
   location = var.location
 
   tags = var.tags
 }
 
-data "azurerm_resource_group" "this" {
-  count = var.create_resource_group ? 0 : 1
-  name  = var.resource_group_name
-}
-
-locals {
-  resource_group = var.create_resource_group ? azurerm_resource_group.this[0] : data.azurerm_resource_group.this[0]
-}
-
 # Manage the network required for the topology.
 module "vnet" {
-  source = "../../modules/vnet"
+  source = "../../../modules/vnet"
 
   for_each = var.vnets
 
   name                   = "${var.name_prefix}${each.value.name}"
   create_virtual_network = each.value.create_virtual_network
-  resource_group_name    = coalesce(each.value.resource_group_name, local.resource_group.name)
+  resource_group_name    = coalesce(each.value.resource_group_name, azurerm_resource_group.this.name)
   location               = var.location
 
   address_space = each.value.address_space
---
+++ tests/modules/vnet/outputs.tf
@@ -0,0 +1,29 @@
+# output "virtual_network_id" {
+#   description = "The identifier of the created or sourced Virtual Network."
+#   value       = local.virtual_network.id
+# }
+
+# output "vnet_cidr" {
+#   description = "VNET address space."
+#   value       = local.virtual_network.address_space
+# }
+
+# output "subnet_ids" {
+#   description = "The identifiers of the created or sourced Subnets."
+#   value       = { for k, v in local.subnets : k => v.id }
+# }
+
+# output "subnet_cidrs" {
+#   description = "Subnet CIDRs (sourced or created)."
+#   value       = { for k, v in local.subnets : k => v.address_prefixes[0] }
+# }
+
+# output "network_security_group_ids" {
+#   description = "The identifiers of the created Network Security Groups."
+#   value       = { for k, v in azurerm_network_security_group.this : k => v.id }
+# }
+
+# output "route_table_ids" {
+#   description = "The identifiers of the created Route Tables."
+#   value       = { for k, v in azurerm_route_table.this : k => v.id }
+# }
