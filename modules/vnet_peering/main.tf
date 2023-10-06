46ae73b88ff121755b1a7099f6265d4b40bbe71f
Adrian Celebanski <acelebanski@paloaltonetworks.com>
2023-10-06 15:56:54 +0200

    Updated the module main, variables, version & MD

--- modules/vnet_peering/main.tf
+++ modules/vnet_peering/main.tf
@@ -9,23 +9,23 @@ data "azurerm_virtual_network" "remote_peer" {
 }
 
 resource "azurerm_virtual_network_peering" "local" {
-  name                         = try(var.local_peer_config.name, "${var.local_peer_config.vnet_name}-to-${var.remote_peer_config.vnet_name}")
+  name                         = var.local_peer_config.name
   resource_group_name          = var.local_peer_config.resource_group_name
   virtual_network_name         = var.local_peer_config.vnet_name
   remote_virtual_network_id    = data.azurerm_virtual_network.remote_peer.id
-  allow_virtual_network_access = try(var.local_peer_config.allow_virtual_network_access, true)
-  allow_forwarded_traffic      = try(var.local_peer_config.allow_forwarded_traffic, true)
-  allow_gateway_transit        = try(var.local_peer_config.allow_gateway_transit, false)
-  use_remote_gateways          = try(var.local_peer_config.use_remote_gateways, false)
+  allow_virtual_network_access = var.local_peer_config.allow_virtual_network_access
+  allow_forwarded_traffic      = var.local_peer_config.allow_forwarded_traffic
+  allow_gateway_transit        = var.local_peer_config.allow_gateway_transit
+  use_remote_gateways          = var.local_peer_config.use_remote_gateways
 }
 
 resource "azurerm_virtual_network_peering" "remote" {
-  name                         = try(var.remote_peer_config.name, "${var.remote_peer_config.vnet_name}-to-${var.local_peer_config.vnet_name}")
+  name                         = var.remote_peer_config.name
   resource_group_name          = var.remote_peer_config.resource_group_name
   virtual_network_name         = var.remote_peer_config.vnet_name
   remote_virtual_network_id    = data.azurerm_virtual_network.local_peer.id
-  allow_virtual_network_access = try(var.remote_peer_config.allow_virtual_network_access, true)
-  allow_forwarded_traffic      = try(var.remote_peer_config.allow_forwarded_traffic, true)
-  allow_gateway_transit        = try(var.remote_peer_config.allow_gateway_transit, false)
-  use_remote_gateways          = try(var.remote_peer_config.use_remote_gateways, false)
+  allow_virtual_network_access = var.remote_peer_config.allow_virtual_network_access
+  allow_forwarded_traffic      = var.remote_peer_config.allow_forwarded_traffic
+  allow_gateway_transit        = var.remote_peer_config.allow_gateway_transit
+  use_remote_gateways          = var.remote_peer_config.use_remote_gateways
 }
